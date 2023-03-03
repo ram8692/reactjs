f38957e536f7d9a19a9bd96d4e6b726757073e2b
rambabu.g <rambabu.g@whatarage.com>
2023-03-03 17:53:37 +0530

    function component

---
+++ src/Components/Man.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+//THERE ARE DIFF WAY TO MAKE FUNC COMP
+//1ST WAY
+/*
+function Man(){
+    return <h2>how are you</h2>;
+}
+export default Man;
+*/
+
+//2ND WAY
+export const Man = () =>{
+    return <h2>how are you</h2>;
+}
+
+
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
+//below code is for 1 way default export
+//import Man from "./Man";
+//import M from "./Man";
 
+
+//below code is for 2 nd way named export
+import {Man} from "./Man";
+
+//file name and class name should be same
 class Person extends Component {
    // constructor() { }
    render(){
@@ -7,6 +15,8 @@ class Person extends Component {
            <div>
                <h3>Name</h3><h3>wscube tech</h3>
                <h3>age</h3><h3>11 years</h3>
+               <Man/>
+               
            </div>
        );
    }
