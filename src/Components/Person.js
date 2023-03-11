0661d2020b2490e05259b3ec7ea2a3521cdbda0b
rambabu.g <rambabu.g@whatarage.com>
2023-03-11 17:28:01 +0530

    how to change state

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -12,13 +12,30 @@ class Person extends Component {
     constructor(props) { 
         super(props);
         this.props = props;
+        this.state = {
+            name:"rammm"
+        }
     }
+
+    changename(){
+        console.log(this.state);
+        this.setState({name:"harion"},()=>{
+            console.log(this.state);
+
+        });
+    }
+
    render(){
        return(
            <div>
+               <h4>name {this.state.name}</h4>
                <h3>Name {Math.random()}{this.props.name} and age is {this.props.age}</h3><h3>wscube tech</h3>
                <h3>age</h3><h3>11 years</h3>
+
                <Man/>
+               <br/>
+               <button onClick={this.changename.bind(this)}>change name</button>
+
                
            </div>
        );
